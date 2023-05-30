9943be52000b737b4b23bd57a378d551ace66ea1
shawon012 <shawon.shakib01@gmail.com>
2023-05-30 09:24:11 +0600

    git is initializing

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+require('dotenv').config()
+const app = express();
+const cors = require('cors');
+const port = process.env.PORT || 5000;
+// middleware
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) =>{
+    res.send('boss is sitting')
+})
+
+app.listen(port, () =>{
+    console.log(`Bistro boss is sitting on port: ${port}`)
+})
+
+
