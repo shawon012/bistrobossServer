6cd7a7dfb0c8a969cc47b54df63505ebed7511b7
shawon012 <shawon.shakib01@gmail.com>
2023-06-01 14:07:51 +0600

    78-3 done

--- index.js
+++ index.js
@@ -32,6 +32,12 @@ async function run() {
 
 
         // user related apis
+
+        app.get('/users', async (req, res) => {
+            const result = await usersCollection.find().toArray();
+            res.send(result);
+          });
+
         app.post('/users', async (req, res) => {
             const user = req.body;
             const query = { email: user.email }
