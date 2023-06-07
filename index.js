dfff783b6f7434e1a3e37c360e3d05ca995e6b15
shawon012 <shawon.shakib01@gmail.com>
2023-06-07 09:09:50 +0600

    index updated

--- index.js
+++ index.js
@@ -127,6 +127,15 @@ async function run() {
             const result = await menuCollection.find().toArray();
             res.send(result)
         })
+
+        app.post('/menu', verifyJWT, verifyAdmin, async (req, res) => {
+            const newItem = req.body;
+            const result = await menuCollection.insertOne(newItem)
+            res.send(result);
+        })
+
+
+
         app.get('/reviews', async (req, res) => {
             const result = await reviewsCollection.find().toArray();
             res.send(result)
